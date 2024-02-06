4643c433cb3f1e94ae279fee5e7a70e861da94ea
Shariful islam <sishajjad@shakti.org.bd>
2024-02-06 19:58:27 +0600

    Popular,Offer,NewCollection  page

--- shopper/src/Pages/Shop.jsx
+++ shopper/src/Pages/Shop.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
 import Popular from "../Components/Popular/Popular";
+import Offers from "../Components/Offers/Offers";
+import NewCollection from "../Components/NewCollections/NewCollection";
 
 function Shop() {
   return (
     <div>
       <Hero />
       <Popular/>
+      <Offers/>
+      <NewCollection/>
     </div>
   );
 }
