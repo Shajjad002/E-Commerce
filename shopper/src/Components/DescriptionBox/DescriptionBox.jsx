c063a3ebb2069ce2f32cbf7d413cb3bc3b1eebad
Shariful islam <sishajjad@shakti.org.bd>
2024-03-04 17:58:48 +0600

    Description Box

--- shopper/src/Components/DescriptionBox/DescriptionBox.jsx
+++ shopper/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -6,6 +6,13 @@ function DescriptionBox() {
         <div className='descriptionBox'>
             <div className="descriptionBox-navigator">
                 <div className="descriptionBox-nav-box">Description Box</div>
+                <div className="descriptionBox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionvox-description">
+                <p>An E-Commerce website is an online platfrom that facilitate.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quaerat repellendus maiores enim eveniet obcaecati nesciunt saepe, aliquid hic dicta voluptates non reprehenderit. Quam nam enim qui, quo veritatis impedit.
+                </p>
+                <p>E-Commerce websites typically display products or services a detailed descriptions,images,prices,and any available</p>
             </div>
 
         </div>
