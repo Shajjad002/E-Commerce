b2b8a32765f6aaa410f55c3676750f382019c6a8
Shariful islam <shajjad002@gmail.com>
2024-03-28 16:51:53 +0600

    cart items

--- shopper/src/Components/CartItem/CartItems.jsx
+++ shopper/src/Components/CartItem/CartItems.jsx
@@ -15,27 +15,30 @@ function CartItems() {
                 <p>Quantity</p>
                 <p>Total</p>
                 <p>Remove</p>
-                <hr />
-                {all_product.map((e)=>{
-                    if(cartItems[e.id]>0)
-                    {
-                        return <div>
-                            <div className="cartitems-format cartitems-format-main">
-                                <img className='carticon-product-icon' src={e.image} alt="" />
-                                <p>{e.name}</p>
-                                <p>${e.new_price}</p>
-                                <button className='cartitem-quantity'>{cartItems[e.id]} </button>
-                                <p>${e.new_price*cartItems[e.id]}</p>
-                                <img className='cartitems-remove-icon' src={remove_icon} alt="" onClick={()=>{removeFromCart(e.id)}} />
-                            </div> 
-                        </div>
-                    }
-                    return null;
+            </div>
+            <hr />
+            {all_product.map((e)=>{
+                if(cartItems[e.id]>0)
+                {
+                    return <div>
+                        <div className="cartitems-format cartitems-format-main">
+                            <img  src={e.image} alt="" className='carticon-product-icon' />
+                            <p>{e.name}</p>
+                            <p>${e.new_price}</p>
+                            <button className='cartitem-quantity'>{cartItems[e.id]} </button>
+                            <p>${e.new_price*cartItems[e.id]}</p>
+                            <img className='cartitems-remove-icon' src={remove_icon} alt="" onClick={()=>{removeFromCart(e.id)}} />
+                        </div> 
+                        <hr />
+                    </div>
+                }
+                return null;
 
-                })}  
-                <div className="cartitems-down">
-                    <div className="cartitems-total">
-                        <h1>Cart Totals</h1>
+            })}  
+            <div className="cartitems-down">
+                <div className="cartitems-total">
+                    <h1>Cart Totals</h1>
+                    <div>
                         <div className="cartitems-total-item">
                             <p>Subtotal</p>
                             <p>${0}</p>
@@ -52,6 +55,14 @@ function CartItems() {
                         </div>
                     </div>
                     <button>PROCEED TO CHECKOUT</button>
+                </div>
+            
+                <div className="cartitems-promocode">
+                    <p>If you have a promo code, Enter it here</p>
+                    <div className="cartitems-promobox">
+                        <input type="text" placeholder='Promo Code' />
+                        <button>Submit</button>
+                    </div>
                 </div>
 
             </div>
