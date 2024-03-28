70d632e6a2bc24b98f837910d54411b2bfc02110
Shariful islam <shajjad002@gmail.com>
2024-03-28 13:19:22 +0600

    cart items

--- shopper/src/Components/CartItem/CartItems.jsx
+++ shopper/src/Components/CartItem/CartItems.jsx
@@ -25,12 +25,33 @@ function CartItems() {
                                 <p>{e.name}</p>
                                 <p>${e.new_price}</p>
                                 <button className='cartitem-quantity'>{cartItems[e.id]} </button>
-                                <p>{e.new_price*cartItems[e.id]}</p>
+                                <p>${e.new_price*cartItems[e.id]}</p>
                                 <img className='cartitems-remove-icon' src={remove_icon} alt="" onClick={()=>{removeFromCart(e.id)}} />
                             </div> 
                         </div>
                     }
+                    return null;
+
                 })}  
+                <div className="cartitems-down">
+                    <div className="cartitems-total">
+                        <h1>Cart Totals</h1>
+                        <div className="cartitems-total-item">
+                            <p>Subtotal</p>
+                            <p>${0}</p>
+                        </div>
+                        <hr />
+                        <div className="cartitems-total-item">
+                            <p>Shipping Fee</p>
+                            <p>Fee</p>
+                        </div>
+                        <hr />
+                        <div className="cartitems-total-item">
+                            <p>Total</p>
+                            <p>${0}</p>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
