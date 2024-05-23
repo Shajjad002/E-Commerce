9acca0b76ef849d1035ae5b7eac7c371486cce17
Shariful islam <shajjad002@gmail.com>
2024-05-23 19:18:11 +0600

    backend file

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors= require("cors");
-const { error } = require("console");
+const { error, log } = require("console");
 const { type } = require("os");
 
 
@@ -84,9 +84,20 @@ const Product= mongoose.model("Product",{
 })
 
 app.post('/addproduct',async(req,res)=>{
+    let products =await Product.find({});
+    let id;
+    if(products.length>0)
+    {
+        let last_product_array= products.slice(-1);
+        let last_product=last_product_array[0];
+        id = last_product.id+1;
+    }
+    else{
+        id=1;
+    }
 
     const product =new Product({
-        id:req.body.id,
+        id:id,
         name:req.body.name,
         image:req.body.image,
         category:req.body.category,
@@ -103,6 +114,25 @@ app.post('/addproduct',async(req,res)=>{
 
 })
 
+//Creating API For Deleting Product
+
+app.post('/removeproduct', async(req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success: true,
+        name: req.body.name,
+    })
+})
+
+
+//Creating API for Getting all products
+
+app.get('/allproducts', async(req,res)=>{
+    let products=  await Product.find({});
+    console.log("All products Fetched");
+    res.send(products);
+})
 
 
 app.listen(port,(error)=>{
