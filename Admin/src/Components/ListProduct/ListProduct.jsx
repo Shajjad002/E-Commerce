0a0554a70bc086a82c5c36ed087301514cabdf0f
Shariful islam <shajjad002@gmail.com>
2024-06-06 14:13:18 +0600

    list product

--- Admin/src/Components/ListProduct/ListProduct.jsx
+++ Admin/src/Components/ListProduct/ListProduct.jsx
@@ -17,6 +17,18 @@ const ListProduct = () => {
       fetchInfo();
     },[])
 
+    const remove_Product= async(id)=>{
+        await fetch ('http://localhost:4000/removeproduct',{
+          method: 'POST',
+          headers:{
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body:JSON.stringify({id:id})
+        })
+        await fetchInfo();
+        
+    }
 
   return (
     <div className='list-product'>
@@ -32,14 +44,18 @@ const ListProduct = () => {
         <div className="listproduct-allproduct">
           <hr />
           {allproducts.map((product,index)=>{
-              return <div key={index} className="listproduct-format-main listproduct-format">
+              return <>
+                <div key={index} className="listproduct-format-main listproduct-format">
                   <img src={product.image} className='listproduct-product-icon' alt="" />
                   <p>{product.name}</p>
                   <p>${product.old_price}</p>
                   <p>${product.new_price}</p>
                   <p>${product.categorye}</p>
-                  <img src={cross_icon} className='listproduct-remove-icon' alt="" />
+                  <img onClick={()=>{remove_Product(product.id)}} src={cross_icon} className='listproduct-remove-icon' alt="" />
               </div>
+             
+              <hr />
+              </>
 
           })}
         </div>
